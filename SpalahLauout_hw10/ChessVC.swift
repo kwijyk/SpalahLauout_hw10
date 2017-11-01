7faa3137251b0714311c9b9372f74688531f3e60
Sergey <ser.olegov@gmail.com>
2017-11-01 17:16:02 +0200

    Work with layout dane

---
+++ SpalahLauout_hw10/ChessVC.swift
@@ -0,0 +1,35 @@
+//
+//  ChessVC.swift
+//  SpalahLauout_hw10
+//
+//  Created by Sergey Gaponov on 11/1/17.
+//  Copyright © 2017 Sergey Gaponov. All rights reserved.
+//
+
+import UIKit
+
+class ChessVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ SpalahLauout_hw10/SmileVC.swift
@@ -0,0 +1,23 @@
+//
+//  SmileVC.swift
+//  SpalahLauout_hw10
+//
+//  Created by Sergey Gaponov on 11/1/17.
+//  Copyright © 2017 Sergey Gaponov. All rights reserved.
+//
+
+import UIKit
+
+class SmileVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func backToSmileVC(sender: UIStoryboardSegue) {
+        
+    }
+
+}
